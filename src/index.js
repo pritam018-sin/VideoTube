617c6ce46fec8ff9f8e52097206cac6229a33304
pritam <wepritam.18@gmail.com>
2025-08-15 10:56:24 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
+import app from './app.js';
 
 dotenv.config({ path: './.env' }); // make sure path is correct
 
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 3000, () => {
+            console.log(`✅ Server is running on http://localhost:${process.env.PORT || 3000}`);
+        });
+    })
+    .catch((error) => {
+        console.error("❌ Database connection failed:", error);
+    });
---
+++ src/utils/apiResponse.js
@@ -0,0 +1,13 @@
+class ApiResponse {
+    constructor(
+        statusCode,
+        message = "Success",
+        data = null
+    ) {
+        this.statusCode = statusCode;
+        this.message = message;
+        this.data = data;
+        this.success = statusCode < 400;
+    }
+}
+export default ApiResponse;
