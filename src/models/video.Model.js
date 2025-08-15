5dbac1c768519b09e9d3603deec7c2ba2a65038a
pritam <wepritam.18@gmail.com>
2025-08-15 12:04:05 +0530

    User model and video model created

---
+++ src/models/video.Model.js
@@ -0,0 +1,62 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, //cloudinary URL
+            required: true
+        },
+        thumbnail: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: Number,
+            required: true
+        },
+        views: {
+            type: Number,
+            default: 0
+        },
+        isPublished: {
+            type: Boolean,
+            default: false
+        },
+
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        likes: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment'
+            }
+        ]
+    },
+    {
+        timestamps: true
+    }
+);
+videoSchema.plugin(mongooseAggregatePaginate);
+
+const Video = mongoose.model('Video', videoSchema);
+
+export default Video;
