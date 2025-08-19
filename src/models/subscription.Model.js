bce508e357474e7732d8f92963ef314492b4f738
pritam <wepritam.18@gmail.com>
2025-08-19 09:54:24 +0530

    Create subscription model with schema for subscriber and channel

---
+++ src/models/subscription.Model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema({
+    subscriber: {
+        type: mongoose.Schema.Types.ObjectId, //ONE WHO IS SUBSCRIBING
+        ref: "User"
+    },
+    channel: {
+        type: mongoose.Schema.Types.ObjectId, //one whom 'subscriber' is subscribing to
+        ref: "User",
+    }
+
+}, 
+{
+    timestamps: true // This will add createdAt and updatedAt fields
+}
+);
+
+const Subscription = mongoose.model("Subscription", subscriptionSchema);
+
+export default Subscription;
