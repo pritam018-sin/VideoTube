b21ac40dd52384c09986c6df45adf63341d59164
pritam <wepritam.18@gmail.com>
2025-08-20 13:31:28 +0530

    Refactor user routes to include additional account management and profile retrieval endpoints

--- src/routes/user.Routes.js
+++ src/routes/user.Routes.js
@@ -1,5 +1,16 @@
 import {Router} from 'express';
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from '../controllers/user.Controller.js';
+import {  changeCurrentPassword,
+          getCurrentUser,
+          getUserChannelProfile,
+          getWatchHistory,
+          loginUser,
+          logoutUser,
+          refreshAccessToken,
+          registerUser,
+          updateAccountDetails,
+          updateUserAvatar,
+          updateUserCoverImage
+} from '../controllers/user.Controller.js';
 import { upload } from '../middlewares/mutler.Middleware.js';
 import { verifyJWT } from '../middlewares/auth.Middleware.js';
 
@@ -20,9 +31,14 @@ router.route('/register').post(
 router.route('/login').post(loginUser);
 
 //secured Routes
-router.route('/logout').post(verifyJWT, logoutUser)
-router.route('/refresh-token').post(refreshAccessToken)
-
-
+router.route('/logout').post(verifyJWT, logoutUser);
+router.route('/refresh-token').post(refreshAccessToken);
+router.route('/change-password').post(verifyJWT, changeCurrentPassword);
+router.route('/current-user').get(verifyJWT, getCurrentUser);
+router.route('/update-account-details').patch(verifyJWT, updateAccountDetails);
+router.route('/update-avatar').patch(verifyJWT,upload.single('avatar'), updateUserAvatar);
+router.route('/update-cover-image').patch(verifyJWT, upload.single('coverImage'), updateUserCoverImage);
+router.route('/channel/:username').get(verifyJWT, getUserChannelProfile);
+router.route('/watch-history').get(verifyJWT, getWatchHistory);
 
 export default router;
